437fe4e8f5251b80fd0f2ce713cb82ded5b391e5
Gzyms69 <dawidekczerwinski@gmail.com>
2026-02-21 16:23:02 +0100

    chore: add Vercel Analytics and Speed Insights, sync pnpm-lock.yaml

--- client/App.tsx
+++ client/App.tsx
@@ -8,6 +8,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Index from "./pages/Index";
 
 import { LanguageProvider } from "./lib/i18n-context";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +20,8 @@ const App = () => (
         <Toaster />
         <Sonner />
         <Index />
+        <Analytics />
+        <SpeedInsights />
       </TooltipProvider>
     </LanguageProvider>
   </QueryClientProvider>
